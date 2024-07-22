bf716e2d0ee851ee6b3209c3076b43d425cc5743
Venkatesh <venka97@gmail.com>
2024-07-22 17:54:55 -0400

    Add paths changes

--- treemonitoring/utils/paths.py
+++ treemonitoring/utils/paths.py
@@ -4,8 +4,8 @@ from pathlib import Path
 
 from treemonitoring.utils.configurable import Configurable
 
-# MILA_HOME = pathlib.Path(__name__).resolve().parents[0]
-MILA_HOME = "/home/mila/v/venkatesh.ramesh/scratch/Tree-Monitoring/"  # TO-DO: Find a better solution to find the config file. Path.resolve is not generalizable
+MILA_HOME = Path(__file__).parent.parent.parent
+# MILA_HOME = "/home/mila/v/venkatesh.ramesh/scratch/Tree-Monitoring/"  # TO-DO: Find a better solution to find the config file. Path.resolve is not generalizable
 
 
 class Paths(Configurable):
@@ -21,18 +21,18 @@ class Paths(Configurable):
 
     def _build(self):
         # location
-        self.paths["application"] = Path(self.scratch["application"])
-        self.paths["temp"] = Path(self.scratch["temp"])
-        self.paths["logs"] = Path(self.scratch["logs"])
+        self.paths["application"] = os.path.join(MILA_HOME, Path(self.scratch["application"]))
+        self.paths["temp"] = os.path.join(MILA_HOME, Path(self.scratch["temp"]))
+        self.paths["logs"] = os.path.join(MILA_HOME, Path(self.scratch["logs"]))
 
         # storage
-        self.paths["datasets"] = Path(self.storage["datasets"])
-        self.paths["archive"] = Path(self.storage["archive"])
+        self.paths["datasets"] = os.path.join(MILA_HOME, Path(self.scratch["datasets"]))
+        self.paths["archive"] = os.path.join(MILA_HOME, Path(self.scratch["archive"]))
 
         # datasets
-        self.paths["quebectrees"] = Path(self.storage["quebectrees"])
-        self.paths["images_dir"] = Path(self.storage["images_dir"])
-        self.paths["class_weights"] = Path(self.storage["class_weights"])
+        self.paths["quebectrees"] = os.path.join(MILA_HOME, Path(self.scratch["quebectrees"]))
+        self.paths["images_dir"] = os.path.join(MILA_HOME, Path(self.scratch["images_dir"]))
+        self.paths["class_weights"] = os.path.join(MILA_HOME, Path(self.scratch["class_weights"]))
 
     def get(self):
         return self.paths
@@ -40,6 +40,4 @@ class Paths(Configurable):
 
 if __name__ == "__main__":
     paths = Paths().get()
-    import ipdb
-
-    ipdb.set_trace()
+    print(paths)
