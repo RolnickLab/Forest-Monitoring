4b097b8274a40ae5efc411231d3222dd6eab9afa
Venkatesh <venka97@gmail.com>
2024-07-23 16:59:32 -0400

    Add changes to setup.py

--- setup.py
+++ setup.py
@@ -2,24 +2,21 @@ from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-keywords = ["segmentation", "deep learning", "tree-monitoring", "dataset", "pytorch", "mila"]
+keywords = ["segmentation", "deep learning", "forest monitoring", "dataset", "pytorch", "mila"]
 
 setup(
     name="treemonitoring",
     version="0.1.0",
     packages=find_packages(),
-    url="https://github.com/RolnickLab/Tree-Monitoring",
+    url="https://github.com/RolnickLab/Forest-Monitoring",
     license="CC BY 4.0",
-    author="Venkatesh Ramesh, Arthur Ouaknine",
+    author="Venkatesh Ramesh, Arthur Ouaknine and David Rolnick",
     author_email="",
     keywords=keywords,
-    description="Forest-monitoring to study the behaviour of trees from spring to leaf-off season.",
+    description="The code repository for our work Tree semantic segmentation from aerial image time series",
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.9",
-    #    install_requires=[
-    #        "torch>=1.13.0",
-    #    ],
     classifiers=[
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
