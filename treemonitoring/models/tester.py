64657c37323844b563bd8c51f922bb3f4e963bfa
Venkatesh <venka97@gmail.com>
2024-08-22 13:07:51 -0400

    Reformat tester.py

--- treemonitoring/models/tester.py
+++ treemonitoring/models/tester.py
@@ -32,24 +32,25 @@ def _load_cfg(cfg_path):
     return cfg
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--cfg", help="Path to config file.")
-parser.add_argument("--ckp", help="Path to checkpoint to load.", default=None)
-parser.add_argument("--savepath", help="Path to checkpoint to load.", default=None)
-parser.add_argument("--debug", help="Debug mode to switch wand mode offline", action="store_true")
-args = parser.parse_args()
-cfg = _load_cfg(args.cfg)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cfg", help="Path to config file.")
+    parser.add_argument("--ckp", help="Path to checkpoint to load.", default=None)
+    parser.add_argument("--savepath", help="Path to save output images.", default=None)
+    parser.add_argument("--debug", help="Debug mode to switch wand mode offline", action="store_true")
+    args = parser.parse_args()
+    cfg = _load_cfg(args.cfg)
 
-if args.debug:
-    os.environ["WANDB_MODE"] = "offline"
+    if args.debug:
+        os.environ["WANDB_MODE"] = "offline"
 
-ckpt_path = args.ckp
-# save_path = "/home/mila/v/venkatesh.ramesh/scratch/images_test/unet_processor_r101/"
-# /home/mila/v/venkatesh.ramesh/scratch/images_test/unet_processor_dice_r101_alternative
-save_path = (
-    "/home/mila/v/venkatesh.ramesh/scratch/images_test/unet_processor_dice_r101_alternative/"
-)
-# save_path = args.savepath
-# "/home/mila/v/venkatesh.ramesh/scratch/images_test"
+    ckpt_path = args.ckp
+    save_path = args.savepath
 
-test_model(cfg["model"]["name"], cfg["dataset"]["n_classes"], ckpt_path, save_path)
+    if save_path:
+        os.makedirs(save_path, exist_ok=True)
+
+    test_model(cfg["model"]["name"], cfg["dataset"]["n_classes"], ckpt_path, save_path)
+
+if __name__ == "__main__":
+    main()
