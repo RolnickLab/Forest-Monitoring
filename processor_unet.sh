d3f3062360e1d7b68952fae28f729b8d032cefee
Venkatesh <venka97@gmail.com>
2024-08-22 13:37:26 -0400

    Remove slurm specific args

--- processor_unet.sh
+++ processor_unet.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-#SBATCH --job-name=treemonitoring
-#SBATCH --output=nohup.out
-#SBATCH --error=error.out
-#SBATCH --ntasks=1
-#SBATCH --time=120:00:00
-#SBATCH --mem=60Gb
-#SBATCH --gres=gpu:rtx8000:1
-#SBATCH --partition=long
-#SBATCH -c 8
-
-module add python/3.9
-source "$HOME/bio/bin/activate"
 
 python treemonitoring/models/trainer.py --cfg treemonitoring/models/configs/processor_unet_7.yaml #--debug
 #python treemonitoring/models/tester.py --cfg treemonitoring/models/configs/processor_unet_7.yaml --ckp PATH_TO_CHECKPOINT --savepath OUTPUT_DIR --debug
--- utae_train.sh
+++ utae_train.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-#SBATCH --job-name=treemonitoring
-#SBATCH --output=nohup.out
-#SBATCH --error=error.out
-#SBATCH --ntasks=1
-#SBATCH --time=72:00:00
-#SBATCH --mem=60Gb
-#SBATCH --gres=gpu:rtx8000:1
-#SBATCH --partition=long
-#SBATCH -c 8
-
-module add python/3.9
-source "$HOME/bio/bin/activate"
 
 python treemonitoring/models/trainer.py --cfg treemonitoring/models/configs/quebectrees_utae_7.yaml  #--debug
